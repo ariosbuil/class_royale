e88fc9da09d791ede52e88c44cfd698f9c2bcac4
JacobsM14 <marcel.esplu@gmail.com>
2024-01-16 13:01:07 +0100

    dunno

--- src/code/pages/management/activity/professor_manage_activity.php
+++ src/code/pages/management/activity/professor_manage_activity.php
@@ -1,3 +1,22 @@
+<?php
+    include '../../../connection.php';
+
+    if(isset($_GET['project_id']))
+    {
+        $id_proyecto = $_GET['project_id'];
+
+
+    }
+    else{
+        // Rdireccionar a la home
+    }
+
+    session_start();
+    $professor_id = $_SESSION['professor_id']
+
+    $query = "SELECT name FROM Activity WHERE project_id = 1";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,7 +30,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;600;700;800;900&display=swap"
         rel="stylesheet">
     <!-- JavaScript -->
-    <script src="./code.js"></script>
+    <!-- <script src="./code.js"></script> -->
     <title>ClassRoyale</title>
 </head>
 
@@ -21,18 +40,6 @@
             <div class="logo">
                 <img src="./../../../../img/logo.png" alt="logo" />
             </div>
-
-            <!-- <div class="search-bar">
-                <div class="search-bar-container-show">
-                    <div class="flex">
-                        <input type="text" placeholder="What do you want?" />
-                        <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24">
-                            <path fill="currentColor"
-                                d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5q0-2.725 1.888-4.612T9.5 3q2.725 0 4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5q0-1.875-1.312-3.187T9.5 5Q7.625 5 6.313 6.313T5 9.5q0 1.875 1.313 3.188T9.5 14" />
-                        </svg>
-                    </div>
-                </div>
-            </div> -->
             <div class="cont-for-header-flex flex">
                 <div class="menu">
                     <ul>
@@ -95,13 +102,43 @@
                 </div>
             </div>
             <div class="show-underline"></div>
-            <!-- FILTER -->
-            <div class="filter-by-projects">
-                <h4>Filter By: </h4>
-                <div class="button-filter-by">
-                    <ul>
-                        <li><a href="">filter</a></li>
-                    </ul>
+            <div class="cont-projects-section">
+                <!-- FILTER -->
+                <div class="filter-by-projects">
+                    <h4>Filter By: </h4>
+                    <div class="button-filter-by">
+                        <ul>
+                            <li><a href="">filter</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- ! CARD PROJECTS GO HERE -->
+                <div class="card-act-show flex">
+                    <div class="card-act-info">
+                        <h3>Activity 1</h3>
+                        <div class="button-skill">
+                            <p>Skill</p>
+                            <p>Skill</p>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio iste necessitatibus iusto, fugit aspernatur rem voluptatem cupiditate accusantium velit amet.</p>
+                    </div>
+                    <div class="edit-delete-options">
+                        <div class="icons-modify flex">
+                            <a href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24">
+                                    <path fill="#ffffff"
+                                        d="M3 21v-4.25L16.2 3.575q.3-.275.663-.425t.762-.15q.4 0 .775.15t.65.45L20.425 5q.3.275.438.65T21 6.4q0 .4-.137.763t-.438.662L7.25 21zM17.6 7.8L19 6.4L17.6 5l-1.4 1.4z" />
+                                </svg>
+                            </a>
+                            <a href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 256 256">
+                                    <path fill="#ffffff"
+                                        d="M228 128a12 12 0 0 1-12 12h-76v76a12 12 0 0 1-24 0v-76H40a12 12 0 0 1 0-24h76V40a12 12 0 0 1 24 0v76h76a12 12 0 0 1 12 12" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
