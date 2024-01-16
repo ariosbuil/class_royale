2d71597483ae22043d4a4c4a2ec3be64c4fa693d
JacobsM14 <marcel.esplu@gmail.com>
2024-01-16 13:14:19 +0100

    dunno 2

--- src/code/pages/home/home_professor.php
+++ src/code/pages/home/home_professor.php
@@ -49,9 +49,10 @@ $result2 = mysqli_query($conn, $query2);
               </svg></a>
             <ul id="dropdown-more-options-content">
               <li><a href="./../home/home_professor.php">Home</a></li>
-              <li><a href="./../management/project/project_management.php">Projects</a></li>
+              <li><a href="./../management//project/project_management.php">Projects</a></li>
               <li><a href="./../management/activity/activity_management.php">Activities</a></li>
               <li><a href="./../management/item/item_management.php">Items</a></li>
+              <li><a href="./../managment/student/student_management.php">Students</a></li>
           </li>
         </ul>
         </ul>
